48c4c77d8862ed0b721f35946c66a6352b3cde65
wzk <wzk_2009@126.com>
2017-05-19 18:27:40 +0800

    add feetTurnToMeters method

--- src/com/wzk/javaDesign10/TestForNo2.java
+++ src/com/wzk/javaDesign10/TestForNo2.java
@@ -8,7 +8,16 @@ import java.util.Scanner;
 public class TestForNo2 {
     public static void main(String[] args){
 //        turnToFahrenheit();
-        areaOfCylinders();
+//        areaOfCylinders();
+        feetTurnToMeters();
+    }
+
+    private static void feetTurnToMeters() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a value for feet:");
+        double feet = input.nextDouble();
+        double meters = feet * 0.305;
+        System.out.println(feet + " feet is " + meters + " meters");
     }
 
     private static void turnToFahrenheit(){
