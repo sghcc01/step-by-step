18229194a5953a96766f4d7b9b9dc26adabc141d
Zhikun.Wang <zhikun.wang@teleyi.com>
2017-05-22 16:01:18 +0800

    add ConputeAndInterpretBMI method

--- src/com/wzk/javaDesign10/TestForNo3.java
+++ src/com/wzk/javaDesign10/TestForNo3.java
@@ -7,12 +7,37 @@ import java.util.Scanner;
  */
 public class TestForNo3 {
     public static void main(String args[]) {
-        practiceSubtraction();
+//        practiceSubtraction();
+        ConputeAndInterpretBMI();
+    }
+
+    private static void ConputeAndInterpretBMI() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter weight in pounds: ");
+        double weight = input.nextDouble();
+        System.out.println("Enter height in inches: ");
+        double height = input.nextDouble();
+        final double KILOGRAMS_PER_POUND = 0.45359237;
+        final double METERS_PER_INCHES = 0.0254;
+        double weightInKilograms = weight * KILOGRAMS_PER_POUND;
+        double heightInMeters = height * METERS_PER_INCHES;
+        double bmi = weightInKilograms / (heightInMeters * heightInMeters);
+        System.out.println("BMI is " + bmi);
+        if (bmi < 18.5) {
+            System.out.println("Underweight" + " 尼玛，太瘦了");
+        }else if (bmi < 25) {
+            System.out.println("Normal" + "！！！告诉你，正常的人没有几个的");
+        }else if (bmi < 30) {
+            System.out.println("Overweight" + " 欢迎你进入超重圈，嘎嘎");
+        }else
+            System.out.println("Obese" + " 大哥，一路走好");
     }
 
     private static void practiceSubtraction() {
         int number1 = (int) (Math.random() * 10);
         int number2 = (int) (Math.random() * 10);
+        double number3 = Math.random();
+        System.out.println(number3);
         if (number1 < number2) {
             int number = number1;
             number1 = number2;
