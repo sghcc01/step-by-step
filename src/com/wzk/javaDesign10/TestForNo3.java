def41819a843576e2c7018c405c0be8fe8829c32
Zhikun.Wang <zhikun.wang@teleyi.com>
2017-05-26 17:42:52 +0800

    update showRandomCoordinates method

--- src/com/wzk/javaDesign10/TestForNo3.java
+++ src/com/wzk/javaDesign10/TestForNo3.java
@@ -16,7 +16,59 @@ public class TestForNo3 {
 //        printISBN10();
 //        showHowManyDaysEachMonth();
 //        verificationPalindrome();
-        guessPositiveOrNegative();
+//        guessPositiveOrNegative();
+//        lottery();
+        showRandomCoordinates();
+    }
+
+    private static void showRandomCoordinates() {
+        int randomCoordinates_x = (int)(Math.random() * 101) - 50;
+        int randomCoordinates_y = (int)(Math.random() * 201) - 100;
+        System.out.println("The random point coordinates is " + "("
+                + randomCoordinates_x + "," + randomCoordinates_y + ")");
+    }
+
+    private static void lottery() {
+        int lottery = (int) (Math.random() * 1000);
+        System.out.println(lottery);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your lottery pick(three digits): ");
+        int guess = input.nextInt();
+        int lotteryDigit1 = lottery / 100;
+        int lotteryDigit2 = (lottery % 100) / 10;
+        int lotteryDigit3 = (lottery % 100) % 10;
+        int guessDigit1 = guess / 100;
+        int guessDigit2 = (guess % 100) / 10;
+        int guessDigit3 = (guess % 100) % 10;
+        if (guess == lottery) {
+            System.out.println("Exact match: you win $10,000");
+        }else if (lotteryDigit1 == guessDigit1
+                && lotteryDigit2 == guessDigit3
+                && lotteryDigit3 == guessDigit2
+                || lotteryDigit1 == guessDigit2
+                && (lotteryDigit2 == guessDigit1
+                && lotteryDigit3 == guessDigit3
+                || lotteryDigit2 == guessDigit3
+                && lotteryDigit3 == guessDigit2
+                || lotteryDigit1 == guessDigit3
+                && (lotteryDigit2 == guessDigit1
+                && lotteryDigit3 == guessDigit2
+                || lotteryDigit2 == guessDigit2
+                && lotteryDigit3 == guessDigit1))) {
+            System.out.println("Match all digits: you win $3,000");
+        }else if (lotteryDigit1 == guessDigit1
+                || lotteryDigit1 == guessDigit2
+                || lotteryDigit1 == guessDigit3
+                || lotteryDigit2 == guessDigit1
+                || lotteryDigit2 == guessDigit2
+                || lotteryDigit2 == guessDigit3
+                || lotteryDigit3 == guessDigit1
+                || lotteryDigit3 == guessDigit2
+                || lotteryDigit3 == guessDigit3) {
+            System.out.println("Match one digit: you win $1,000. the lottery is " + lottery);
+        } else {
+            System.out.println("Sorry,no match. the lottery is " + lottery);
+        }
     }
 
     private static void guessPositiveOrNegative() {
