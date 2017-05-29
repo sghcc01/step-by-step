05787820e606e890226fae54e700918bc08f59dc
wzk <wzk_2009@126.com>
2017-05-29 20:28:27 +0800

    add calculateMaximumCircleDistance method (4.2)

--- src/com/wzk/javaDesign10/TestForNo4.java
+++ src/com/wzk/javaDesign10/TestForNo4.java
@@ -7,7 +7,34 @@ import java.util.Scanner;
  */
 public class TestForNo4 {
     public static void main(String[] args) {
-        calculateAreaOfPentagon();
+//        calculateAreaOfPentagon();
+        calculateMaximumCircleDistance();
+    }
+
+    private static void calculateMaximumCircleDistance() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter point 1 (latitude and longitude) in degrees: ");
+        String point1 = input.nextLine();
+        System.out.println("Enter point 2 (latitude and longitude) in degrees: ");
+        String point2 = input.nextLine();
+        int segmentation1 = point1.indexOf(',');
+        int segmentation2 = point2.indexOf(',');
+//        String x1 = point1.substring(0,segmentation);
+//        String y1 = point2.substring(segmentation + 1);
+//        String x2 = point1.substring(0,segmentation);
+//        String y2 = point2.substring(segmentation + 1);
+        double x1 = Double.parseDouble(point1.substring(0,segmentation1));
+        double y1 = Double.parseDouble(point1.substring(segmentation1 + 1));
+        double x2 = Double.parseDouble(point2.substring(0,segmentation2));
+        double y2 = Double.parseDouble(point2.substring(segmentation2 + 1));
+        final double r = 6371.03;
+        double d = r * Math.acos(Math.sin(Math.toRadians(x1))
+                                 * Math.sin(Math.toRadians(x2))
+                                 + Math.cos(Math.toRadians(x1))
+                                 * Math.cos(Math.toRadians(x2))
+                                 * Math.cos(Math.toRadians(y1) - Math.toRadians(y2)));
+        System.out.println(d);
+        System.out.printf("The distance between the two points is %f km",d);
     }
 
     private static void calculateAreaOfPentagon() {
