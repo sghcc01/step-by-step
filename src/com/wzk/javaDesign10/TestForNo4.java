35052f434d4b6134812c48fc3f9c26f5472c8d37
wzk <wzk_2009@126.com>
2017-05-30 22:45:52 +0800

    add calculateAreaOfTriangle method (4.6)

--- src/com/wzk/javaDesign10/TestForNo4.java
+++ src/com/wzk/javaDesign10/TestForNo4.java
@@ -2,13 +2,33 @@ package com.wzk.javaDesign10;
 
 import java.util.Scanner;
 
-/**
- * Created by admin on 2017/5/27.
- */
 public class TestForNo4 {
     public static void main(String[] args) {
 //        calculateAreaOfPentagon();
-        calculateMaximumCircleDistance();
+//        calculateMaximumCircleDistance();
+        calculateAreaOfTriangle();
+    }
+
+    private static void calculateAreaOfTriangle() {
+        double angle1 = Math.random() * Math.PI * 2;
+        double angle2 = Math.random() * Math.PI * 2;
+        double angle3 = Math.random() * Math.PI * 2;
+        final int r = 40;
+        //算出三个点的坐标，分别是（x1,y1）...
+        double angle_x1 = r * Math.cos(angle1);
+        double angle_y1 = r * Math.sin(angle1);
+        double angle_x2 = r * Math.cos(angle2);
+        double angle_y2 = r * Math.sin(angle2);
+        double angle_x3 = r * Math.cos(angle3);
+        double angle_y3 = r * Math.sin(angle3);
+        //算出三边长度,(x1,y1)形成的角对应的边设为a，以此类推
+        double a = Math.pow(Math.pow(angle_x3 - angle_x2, 2) + Math.pow(angle_y3 - angle_y2, 2), 0.5);
+        double b = Math.pow(Math.pow(angle_x3 - angle_x1, 2) + Math.pow(angle_y3 - angle_y1, 2), 0.5);
+        double c = Math.pow(Math.pow(angle_x2 - angle_x1, 2) + Math.pow(angle_y2 - angle_y1, 2), 0.5);
+        double a_degree = Math.acos((a * a - b * b - c * c) / (-2 * b * c));
+        double b_degree = Math.acos((-a * a + b * b - c * c) / (-2 * c * a));
+        double c_degree = Math.acos((-a * a - b * b + c * c) / (-2 * b * a));
+        System.out.printf("三个角的度数分别是%.2f度,%.2f度,%.2f度",Math.toDegrees(a_degree),Math.toDegrees(b_degree),Math.toDegrees(c_degree));
     }
 
     private static void calculateMaximumCircleDistance() {
