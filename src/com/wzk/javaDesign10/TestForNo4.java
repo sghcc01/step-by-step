0edb9dd2f366bff7fc4c6257576b291b4ceefbad
Zhikun.Wang <zhikun.wang@teleyi.com>
2017-05-31 17:11:13 +0800

    add exercise04_22 method

--- src/com/wzk/javaDesign10/TestForNo4.java
+++ src/com/wzk/javaDesign10/TestForNo4.java
@@ -16,7 +16,22 @@ public class TestForNo4 {
 //        showRandomUppercaseLetter();
 //        showHowManyDays();
 //        showLenghtAndFirstCharacter();
-        checkSSN();
+//        checkSSN();
+        exercise04_22();
+    }
+
+    private static void exercise04_22() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter string s1: ");
+        String s1 = input.nextLine();
+        System.out.println("Enter string s2: ");
+        String s2 = input.nextLine();
+        if (s1.indexOf(s2) != -1) {
+            System.out.println(s2 + " is a substring of " + s1);
+        } else {
+            System.out.println(s2 + " is not a substring of " + s1);
+        }
+
     }
 
     private static void checkSSN() {
